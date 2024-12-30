4e01416d82d0aba35628d875675185fa6f245ad1
Tirth Mewada <tirthmewada12345@gmail.com>
2024-12-30 17:41:20 +0530

    first commit

---
+++ assests/main.js
@@ -0,0 +1,142 @@
+/*===== MENU SHOW =====*/ 
+const showMenu = (toggleId, navId) =>{
+    const toggle = document.getElementById(toggleId),
+    nav = document.getElementById(navId)
+
+    if(toggle && nav){
+        toggle.addEventListener('click', ()=>{
+            nav.classList.toggle('show')
+        })
+    }
+}
+showMenu('nav-toggle','nav-menu')
+
+/*==================== REMOVE MENU MOBILE ====================*/
+const navLink = document.querySelectorAll('.nav__link')
+
+function linkAction(){
+    const navMenu = document.getElementById('nav-menu')
+    // When we click on each nav__link, we remove the show-menu class
+    navMenu.classList.remove('show')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
+
+/*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
+const sections = document.querySelectorAll('section[id]')
+
+const scrollActive = () =>{
+    const scrollDown = window.scrollY
+
+  sections.forEach(current =>{
+        const sectionHeight = current.offsetHeight,
+              sectionTop = current.offsetTop - 58,
+              sectionId = current.getAttribute('id'),
+              sectionsClass = document.querySelector('.nav__menu a[href*=' + sectionId + ']')
+        
+        if(scrollDown > sectionTop && scrollDown <= sectionTop + sectionHeight){
+            sectionsClass.classList.add('active-link')
+        }else{
+            sectionsClass.classList.remove('active-link')
+        }                                                    
+    })
+}
+window.addEventListener('scroll', scrollActive)
+
+/*===== SCROLL REVEAL ANIMATION =====*/
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '60px',
+    duration: 2000,
+    delay: 200,
+//     reset: true
+});
+
+sr.reveal('.home__data, .about__img, .skills__subtitle, .skills__text',{}); 
+sr.reveal('.home__img, .about__subtitle, .about__text, .skills__img',{delay: 400}); 
+sr.reveal('.home__social-icon',{ interval: 200}); 
+sr.reveal('.skills__data, .work__img, .contact__input',{interval: 200}); 
+
+
+
+
+
+document.addEventListener("DOMContentLoaded", function() { 
+    const carousel = document.querySelector(".carousel"); 
+    const arrowBtns = document.querySelectorAll(".wrapper i"); 
+    const wrapper = document.querySelector(".wrapper"); 
+  
+    const firstCard = carousel.querySelector(".card"); 
+    const firstCardWidth = firstCard.offsetWidth; 
+  
+    let isDragging = false, 
+        startX, 
+        startScrollLeft, 
+        timeoutId; 
+  
+    const dragStart = (e) => {  
+        isDragging = true; 
+        carousel.classList.add("dragging"); 
+        startX = e.pageX; 
+        startScrollLeft = carousel.scrollLeft; 
+    }; 
+  
+    const dragging = (e) => { 
+        if (!isDragging) return; 
+      
+        // Calculate the new scroll position 
+        const newScrollLeft = startScrollLeft - (e.pageX - startX); 
+      
+        // Check if the new scroll position exceeds  
+        // the carousel boundaries 
+        if (newScrollLeft <= 0 || newScrollLeft >=  
+            carousel.scrollWidth - carousel.offsetWidth) { 
+              
+            // If so, prevent further dragging 
+            isDragging = false; 
+            return; 
+        } 
+      
+        // Otherwise, update the scroll position of the carousel 
+        carousel.scrollLeft = newScrollLeft; 
+    }; 
+  
+    const dragStop = () => { 
+        isDragging = false;  
+        carousel.classList.remove("dragging"); 
+    }; 
+  
+    const autoPlay = () => { 
+      
+        // Return if window is smaller than 800 
+        if (window.innerWidth < 800) return;  
+          
+        // Calculate the total width of all cards 
+        const totalCardWidth = carousel.scrollWidth; 
+          
+        // Calculate the maximum scroll position 
+        const maxScrollLeft = totalCardWidth - carousel.offsetWidth; 
+          
+        // If the carousel is at the end, stop autoplay 
+        if (carousel.scrollLeft >= maxScrollLeft) return; 
+          
+        // Autoplay the carousel after every 2500ms 
+        timeoutId = setTimeout(() =>  
+            carousel.scrollLeft += firstCardWidth, 2500); 
+    }; 
+  
+    carousel.addEventListener("mousedown", dragStart); 
+    carousel.addEventListener("mousemove", dragging); 
+    document.addEventListener("mouseup", dragStop); 
+    wrapper.addEventListener("mouseenter", () =>  
+        clearTimeout(timeoutId)); 
+    wrapper.addEventListener("mouseleave", autoPlay); 
+  
+    // Add event listeners for the arrow buttons to  
+    // scroll the carousel left and right 
+    arrowBtns.forEach(btn => { 
+        btn.addEventListener("click", () => { 
+            carousel.scrollLeft += btn.id === "left" ?  
+                -firstCardWidth : firstCardWidth; 
+        }); 
+    }); 
+});
